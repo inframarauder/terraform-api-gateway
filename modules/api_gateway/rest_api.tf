57b0807d0b646fdac0e2d4da7be942e9a3f7dae8
subhasis020299 <subhasisdas125@gmail.com>
2022-04-02 22:36:42 +0530

    added a mock integration request and response

--- modules/api_gateway/rest_api.tf
+++ modules/api_gateway/rest_api.tf
@@ -8,9 +8,43 @@ resource "aws_api_gateway_resource" "rest_api_resource" {
   path_part = "movies"
 }
 
-resource "aws_api_gateway_method" "rest_api_method"{
+resource "aws_api_gateway_method" "rest_api_get_method"{
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   resource_id = aws_api_gateway_resource.rest_api_resource.id
   http_method = "GET"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "rest_api_get_method_integration" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.rest_api_resource.id
+  http_method = aws_api_gateway_method.rest_api_get_method.http_method
+  type = "MOCK"
+  //request_tempates is required to explicitly set the statusCode to an integer value of 200
+   request_templates = {
+    "application/json" = jsonencode({
+      statusCode = 200
+    })
+  }
+}
+
+resource "aws_api_gateway_method_response" "rest_api_get_method_response_200"{
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.rest_api_resource.id
+  http_method = aws_api_gateway_method.rest_api_get_method.http_method
+  status_code = "200"
+ 
+  
+}
+
+resource "aws_api_gateway_integration_response" "rest_api_get_method_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.rest_api_resource.id
+  http_method = aws_api_gateway_integration.rest_api_get_method_integration.http_method
+  status_code = aws_api_gateway_method_response.rest_api_get_method_response_200.status_code
+  response_templates = {
+    "application/json" = jsonencode({
+      body = "Hello from the movies API!"
+    })
+  }
+} 
