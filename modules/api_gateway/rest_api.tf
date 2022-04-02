78bf3e398e89bbb0773ab8f71e9b117749ff7021
subhasis020299 <subhasisdas125@gmail.com>
2022-04-02 21:23:38 +0530

    added GET method on movies resource

--- modules/api_gateway/rest_api.tf
+++ modules/api_gateway/rest_api.tf
@@ -7,3 +7,10 @@ resource "aws_api_gateway_resource" "rest_api_resource" {
   parent_id = aws_api_gateway_rest_api.rest_api.root_resource_id
   path_part = "movies"
 }
+
+resource "aws_api_gateway_method" "rest_api_method"{
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.rest_api_resource.id
+  http_method = "GET"
+  authorization = "NONE"
+}
