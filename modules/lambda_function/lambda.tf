99a58db71423bb9e723506560f0a723c911c8bf6
subhasis020299 <subhasisdas125@gmail.com>
2022-04-03 00:02:35 +0530

    added acrhive_file data source to zip

--- modules/lambda_function/lambda.tf
+++ modules/lambda_function/lambda.tf
@@ -1,9 +1,19 @@
+//using archive_file data source to zip the lambda code:
+data "archive_file" "lambda_code" {
+ type = "zip"
+ source_dir = "${path.module}/function_code"
+ output_path = "${path.module}/function_code.zip"
+}
+
+
+//creating the s3 bucket to store the lambda code:
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = var.s3_bucket_name
 }
 
-//making the s3 bucket private as it houses the lambda code
+//making the s3 bucket private as it houses the lambda code:
 resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
   bucket = aws_s3_bucket.lambda_bucket.id
   acl = "private"
 }
+
