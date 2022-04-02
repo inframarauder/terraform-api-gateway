39f490f4c866821861404c6bc8672623a493e7df
subhasis020299 <subhasisdas125@gmail.com>
2022-04-02 13:13:13 +0530

    added rest_api_resource alongwith output

---
+++ modules/api_gateway/outputs.tf
@@ -0,0 +1,3 @@
+output "rest_api_resource_path"{
+    value = aws_api_gateway_resource.rest_api_resource.path
+}
--- modules/api_gateway/rest_api.tf
+++ modules/api_gateway/rest_api.tf
@@ -1,3 +1,9 @@
 resource "aws_api_gateway_rest_api" "rest_api"{
     name = var.rest_api_name
 }
+
+resource "aws_api_gateway_resource" "rest_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part = "movies"
+}
